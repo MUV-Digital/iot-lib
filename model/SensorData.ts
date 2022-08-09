4f9a82564564e09583e2adf3fe2ad9f618069472
frooner <marco@muv-k.ch>
2022-08-09 14:14:21 +0200

    sensor data is gobject

--- model/SensorData.ts
+++ model/SensorData.ts
@@ -1,5 +1,10 @@
-export interface SensorData {
-  id: string;
+import { Gobject } from './Gobject';
+
+export type SensorData = Gobject<Property, Relation>;
+
+interface Property {
   value: number | boolean;
   timestamp: number;
 }
+
+interface Relation {}

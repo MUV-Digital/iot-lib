b1dc6787c22619189777a4e957c42008e6cc4205
frooner <marco@muv-k.ch>
2022-08-25 09:58:57 +0200

    export action from state file

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export type { State } from './model/State';
+export type { State, Action } from './model/State';
 export type { Sensor } from './model/Sensor';
 export type { Device } from './model/Device';
 export type { Customer } from './model/Customer';

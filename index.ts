ef3dd0e4f194ee0b99d4c9c3dc82edc8c263b46a
frooner <marco@muv-k.ch>
2022-08-29 16:43:13 +0200

    sensor type as enum

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ export type { State, Action } from './model/State';
 export type { Sensor } from './model/Sensor';
 export type { Device } from './model/Device';
 export type { Customer } from './model/Customer';
-export type { SensorType } from './model/SensorType';
+export { SensorType } from './model/SensorType';
